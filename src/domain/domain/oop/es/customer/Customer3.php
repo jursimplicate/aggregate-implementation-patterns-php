11f3738a5c0ee0939c74f49c3549ecd222f227c9
Jur de Vries <jur.devries@simplicate.nl>
2022-08-11 13:31:08 +0200

    Added OOP es classes.

---
+++ src/domain/domain/oop/es/customer/Customer3.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace domain\domain\oop\es\customer;
+
+use domain\shared\command\ChangeCustomerEmailAddress;
+use domain\shared\command\ConfirmCustomerEmailAddress;
+use domain\shared\command\RegisterCustomer;
+use domain\shared\event\CustomerEmailAddressChanged;
+use domain\shared\event\CustomerEmailAddressConfirmed;
+use domain\shared\event\CustomerRegistered;
+use domain\shared\event\Event;
+use domain\shared\value\EmailAddress;
+use domain\shared\value\Hash;
+use domain\shared\value\PersonName;
+
+class Customer3
+{
+    private ?EmailAddress $emailAddress;
+    private ?Hash $confirmationHash;
+    private bool $isEmailAdressConfirmed;
+    private ?PersonName $name;
+
+    private function __construct()
+    {
+    }
+
+    public static function register(RegisterCustomer $command): ?CustomerRegistered
+    {
+        // TODO
+        return null;
+    }
+
+    /**
+     * @param array<Event> $events
+     */
+    public function reconstitute(array $events): Customer3
+    {
+        $customer = new Customer3();
+
+        $customer->apply($events);
+
+        return $customer;
+    }
+
+    /**
+     * @param ConfirmCustomerEmailAddress $command
+     *
+     * @return array<Event>
+     */
+    public function confirmEmailAddress(ConfirmCustomerEmailAddress $command): array
+    {
+        // TODO
+
+        return [];
+    }
+
+    /**
+     * @param ConfirmCustomerEmailAddress $command
+     *
+     * @return array<Event>
+     */
+    public function changeEmailAddress(ChangeCustomerEmailAddress $command): array
+    {
+        // TODO
+
+        return [];
+    }
+
+    /**
+     * @param array<Event> $events
+     */
+    private function apply(array $events)
+    {
+        foreach ($events as $event) {
+            $this->applyEvent($event);
+        }
+    }
+
+    private function applyEvent(Event $event): void
+    {
+        if ($event instanceof CustomerRegistered) {
+            // TODO
+        }
+        elseif ($event instanceof CustomerEmailAddressConfirmed) {
+            // TODO
+        }
+        elseif ($event instanceof CustomerEmailAddressChanged) {
+            // TODO
+        }
+    }
+}
---
+++ src/domain/domain/oop/es/customer/Customer4.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace domain\domain\oop\es\customer;
+
+use domain\shared\command\ChangeCustomerEmailAddress;
+use domain\shared\command\ConfirmCustomerEmailAddress;
+use domain\shared\command\RegisterCustomer;
+use domain\shared\event\CustomerEmailAddressChanged;
+use domain\shared\event\CustomerEmailAddressConfirmed;
+use domain\shared\event\CustomerRegistered;
+use domain\shared\event\Event;
+use domain\shared\value\EmailAddress;
+use domain\shared\value\Hash;
+use domain\shared\value\PersonName;
+
+class Customer4
+{
+    private ?EmailAddress $emailAddress;
+    private ?Hash $confirmationHash;
+    private bool $isEmailAdressConfirmed;
+    private ?PersonName $name;
+
+    private array $recordedEvents;
+
+    private function __construct()
+    {
+        $this->recordedEvents = [];
+    }
+
+    public static function register(RegisterCustomer $command): Customer4
+    {
+        $customer = new Customer4();
+
+        // TODO
+
+        return $customer;
+    }
+
+    /**
+     * @param array<Event> $events
+     */
+    public function reconstitute(array $events): Customer4
+    {
+        $customer = new Customer4();
+
+        $customer->apply($events);
+
+        return $customer;
+    }
+
+    public function confirmEmailAddress(ConfirmCustomerEmailAddress $command): void
+    {
+        // TODO
+    }
+
+    public function changeEmailAddress(ChangeCustomerEmailAddress $command): void
+    {
+        // TODO
+    }
+
+    /**
+     * @return array<Event>
+     */
+    public function getRecordedEvents(): array
+    {
+        return $this->recordedEvents;
+    }
+
+    private function recordThat(Event $event): void
+    {
+        $this->recordedEvents[] = $event;
+    }
+
+
+    /**
+     * @param array<Event> $events
+     */
+    private function apply(array $events)
+    {
+        foreach ($events as $event) {
+            $this->applyEvent($event);
+        }
+    }
+
+    private function applyEvent(Event $event): void
+    {
+        if ($event instanceof CustomerRegistered) {
+            // TODO
+        }
+        elseif ($event instanceof CustomerEmailAddressConfirmed) {
+            // TODO
+        }
+        elseif ($event instanceof CustomerEmailAddressChanged) {
+            // TODO
+        }
+    }
+}
